06f165d6abf553bb5c9f55ca02e8d5a5e570bd03
Mr. REP <45342092+XOGDEV@users.noreply.github.com>
2021-11-13 11:48:46 +0700

    Add files via upload

---
+++ TransaksiPeminjamanBuku/Form1.Designer.vb
@@ -0,0 +1,333 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.txtAnggota = New System.Windows.Forms.TextBox()
+        Me.txtKTP = New System.Windows.Forms.TextBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtBukudipinjam = New System.Windows.Forms.TextBox()
+        Me.txtJumlahKeterlambatan = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.txtTotalDenda = New System.Windows.Forms.TextBox()
+        Me.txtPinjamBuku = New System.Windows.Forms.TextBox()
+        Me.txtTotalBayar = New System.Windows.Forms.TextBox()
+        Me.txtUangDibayarkan = New System.Windows.Forms.TextBox()
+        Me.txtKembalian = New System.Windows.Forms.TextBox()
+        Me.cmdHasil = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(130, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(222, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Transaksi Peminjaman Buku"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "No Anggota"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(39, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "No. KTP"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(39, 106)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Nama Lengkap"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(39, 132)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(164, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Nama Buku Yang Akan di Pinjam"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(39, 161)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(106, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Tanggal Peminjaman"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(39, 187)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(116, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Tanggal Pengembalian"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(39, 210)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(139, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Jumlah Keterlambatan (Hari)"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(39, 236)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(95, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Total Biaya Denda"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(39, 262)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(124, 13)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "Harga Peminjaman Buku"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(39, 288)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(61, 13)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "Total Bayar"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(39, 314)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(120, 13)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "Uang Yang di Bayarkan"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(39, 340)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(56, 13)
+        Me.Label13.TabIndex = 12
+        Me.Label13.Text = "Kembalian"
+        '
+        'txtAnggota
+        '
+        Me.txtAnggota.Location = New System.Drawing.Point(252, 51)
+        Me.txtAnggota.Name = "txtAnggota"
+        Me.txtAnggota.Size = New System.Drawing.Size(200, 20)
+        Me.txtAnggota.TabIndex = 13
+        '
+        'txtKTP
+        '
+        Me.txtKTP.Location = New System.Drawing.Point(252, 77)
+        Me.txtKTP.Name = "txtKTP"
+        Me.txtKTP.Size = New System.Drawing.Size(200, 20)
+        Me.txtKTP.TabIndex = 14
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(252, 103)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(200, 20)
+        Me.txtNama.TabIndex = 15
+        '
+        'txtBukudipinjam
+        '
+        Me.txtBukudipinjam.Location = New System.Drawing.Point(252, 129)
+        Me.txtBukudipinjam.Name = "txtBukudipinjam"
+        Me.txtBukudipinjam.Size = New System.Drawing.Size(200, 20)
+        Me.txtBukudipinjam.TabIndex = 16
+        '
+        'txtJumlahKeterlambatan
+        '
+        Me.txtJumlahKeterlambatan.Location = New System.Drawing.Point(252, 207)
+        Me.txtJumlahKeterlambatan.Name = "txtJumlahKeterlambatan"
+        Me.txtJumlahKeterlambatan.Size = New System.Drawing.Size(200, 20)
+        Me.txtJumlahKeterlambatan.TabIndex = 17
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(252, 155)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 18
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Location = New System.Drawing.Point(252, 181)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker2.TabIndex = 19
+        '
+        'txtTotalDenda
+        '
+        Me.txtTotalDenda.Location = New System.Drawing.Point(252, 233)
+        Me.txtTotalDenda.Name = "txtTotalDenda"
+        Me.txtTotalDenda.Size = New System.Drawing.Size(200, 20)
+        Me.txtTotalDenda.TabIndex = 20
+        '
+        'txtPinjamBuku
+        '
+        Me.txtPinjamBuku.Location = New System.Drawing.Point(252, 259)
+        Me.txtPinjamBuku.Name = "txtPinjamBuku"
+        Me.txtPinjamBuku.Size = New System.Drawing.Size(200, 20)
+        Me.txtPinjamBuku.TabIndex = 21
+        '
+        'txtTotalBayar
+        '
+        Me.txtTotalBayar.Location = New System.Drawing.Point(252, 285)
+        Me.txtTotalBayar.Name = "txtTotalBayar"
+        Me.txtTotalBayar.Size = New System.Drawing.Size(200, 20)
+        Me.txtTotalBayar.TabIndex = 22
+        '
+        'txtUangDibayarkan
+        '
+        Me.txtUangDibayarkan.Location = New System.Drawing.Point(252, 311)
+        Me.txtUangDibayarkan.Name = "txtUangDibayarkan"
+        Me.txtUangDibayarkan.Size = New System.Drawing.Size(200, 20)
+        Me.txtUangDibayarkan.TabIndex = 23
+        '
+        'txtKembalian
+        '
+        Me.txtKembalian.Location = New System.Drawing.Point(252, 337)
+        Me.txtKembalian.Name = "txtKembalian"
+        Me.txtKembalian.Size = New System.Drawing.Size(200, 20)
+        Me.txtKembalian.TabIndex = 24
+        '
+        'cmdHasil
+        '
+        Me.cmdHasil.Location = New System.Drawing.Point(42, 373)
+        Me.cmdHasil.Name = "cmdHasil"
+        Me.cmdHasil.Size = New System.Drawing.Size(410, 28)
+        Me.cmdHasil.TabIndex = 25
+        Me.cmdHasil.Text = "Hasil"
+        Me.cmdHasil.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(507, 413)
+        Me.Controls.Add(Me.cmdHasil)
+        Me.Controls.Add(Me.txtKembalian)
+        Me.Controls.Add(Me.txtUangDibayarkan)
+        Me.Controls.Add(Me.txtTotalBayar)
+        Me.Controls.Add(Me.txtPinjamBuku)
+        Me.Controls.Add(Me.txtTotalDenda)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.txtJumlahKeterlambatan)
+        Me.Controls.Add(Me.txtBukudipinjam)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.txtKTP)
+        Me.Controls.Add(Me.txtAnggota)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(523, 452)
+        Me.MinimumSize = New System.Drawing.Size(523, 452)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Transaksi Peminjaman Buku"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents txtAnggota As System.Windows.Forms.TextBox
+    Friend WithEvents txtKTP As System.Windows.Forms.TextBox
+    Friend WithEvents txtNama As System.Windows.Forms.TextBox
+    Friend WithEvents txtBukudipinjam As System.Windows.Forms.TextBox
+    Friend WithEvents txtJumlahKeterlambatan As System.Windows.Forms.TextBox
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents DateTimePicker2 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents txtTotalDenda As System.Windows.Forms.TextBox
+    Friend WithEvents txtPinjamBuku As System.Windows.Forms.TextBox
+    Friend WithEvents txtTotalBayar As System.Windows.Forms.TextBox
+    Friend WithEvents txtUangDibayarkan As System.Windows.Forms.TextBox
+    Friend WithEvents txtKembalian As System.Windows.Forms.TextBox
+    Friend WithEvents cmdHasil As System.Windows.Forms.Button
+
+End Class
---
+++ TransaksiPeminjamanBuku/Form1.vb
@@ -0,0 +1,177 @@
+﻿Public Class Form1
+    Sub info()
+        Beep()
+        MessageBox.Show("Created by XOGDEV" & vbCrLf & "NAMA: XOG" & vbCrLf & "NPM: **********", "Alert")
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        info()
+
+        TopMost = False
+        cmdHasil.Enabled = False
+
+        DateTimePicker1.Value = Now
+        DateTimePicker2.Value = Now
+
+        txtJumlahKeterlambatan.ReadOnly = True
+        txtJumlahKeterlambatan.BackColor = Color.White
+        txtTotalDenda.ReadOnly = True
+        txtTotalDenda.BackColor = Color.White
+        txtTotalBayar.ReadOnly = True
+        txtTotalBayar.BackColor = Color.White
+        txtKembalian.ReadOnly = True
+        txtKembalian.BackColor = Color.White
+
+        txtPinjamBuku.Text = ""
+        txtJumlahKeterlambatan.Text = ""
+        txtTotalDenda.Text = ""
+        txtUangDibayarkan.Text = ""
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DateTimePicker1.ValueChanged
+        Dim biaya As Integer
+        Dim keterlambatan As Integer
+        Dim hasil As Integer
+
+        If True Then
+            Dim dt1 As DateTime = Convert.ToDateTime(DateTimePicker1.Text)
+            Dim dt2 As DateTime = Convert.ToDateTime(DateTimePicker2.Text)
+            Dim ts As TimeSpan = dt1.Subtract(dt2)
+
+            If Convert.ToInt32(ts.Days) <= 7 Then
+                biaya = "2000" 'Rp. 2.000
+            ElseIf Convert.ToInt32(ts.Days) <= 14 Then
+                biaya = "7500" 'Rp. 7.500
+            ElseIf Convert.ToInt32(ts.Days) <= 30 Then
+                biaya = "50000" 'Rp. 50.000
+            End If
+            txtJumlahKeterlambatan.Text = ts.Days
+            keterlambatan = Val(txtJumlahKeterlambatan.Text)
+            hasil = biaya * keterlambatan
+            txtTotalDenda.Text = Format(hasil, "##,##0")
+        End If
+    End Sub
+
+    Private Sub DateTimePicker2_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DateTimePicker2.ValueChanged
+        Dim biaya As Integer
+        Dim keterlambatan As Integer
+        Dim hasil As Integer
+
+        If True Then
+            Dim dt1 As DateTime = Convert.ToDateTime(DateTimePicker1.Text)
+            Dim dt2 As DateTime = Convert.ToDateTime(DateTimePicker2.Text)
+            Dim ts As TimeSpan = dt2.Subtract(dt1)
+
+            If Convert.ToInt32(ts.Days) <= 7 Then
+                biaya = "2000" 'Rp. 2.000
+            ElseIf Convert.ToInt32(ts.Days) <= 14 Then
+                biaya = "7500" 'Rp. 7.500
+            ElseIf Convert.ToInt32(ts.Days) <= 30 Then
+                biaya = "50000" 'Rp. 50.000
+            End If
+            txtJumlahKeterlambatan.Text = ts.Days
+            keterlambatan = Val(txtJumlahKeterlambatan.Text)
+            hasil = biaya * keterlambatan
+            txtTotalDenda.Text = Format(hasil, "##,##0")
+        End If
+    End Sub
+
+    Private Sub txtcmdHasil_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdHasil.Click
+        'Dim TotalBayar As Integer
+        'Dim Kembalian As Integer
+        Dim Tunai As Double
+
+        If txtAnggota.Text = String.Empty Then
+            MsgBox("No. Anggota Mohon Di isi")
+        ElseIf txtKTP.Text = String.Empty Then
+            MsgBox("No. KTP Mohon Di isi")
+        ElseIf txtNama.Text = String.Empty Then
+            MsgBox("Nama Lengkap Mohon Di isi")
+        ElseIf txtBukudipinjam.Text = String.Empty Then
+            MsgBox("Nama Buku yang akan di Pinjam Mohon Di isi")
+        ElseIf txtUangDibayarkan.Text = String.Empty Then
+            MsgBox("Uang yang akan di Bayarkan Mohon Di isi")
+        ElseIf txtPinjamBuku.Text = String.Empty Then
+            MsgBox("Harga Pinjam Buku Mohon Di isi")
+        Else
+            Tunai = txtUangDibayarkan.Text
+            txtKembalian.Text = Format(Tunai, "##,##0")
+            txtKembalian.Text = Tunai - txtTotalBayar.Text
+            'Kembalian = Val(txtUangDibayarkan.Text) - Val(txtTotalBayar.Text)
+            'txtKembalian.Text = Format(Kembalian, "##,##0")
+        End If
+    End Sub
+
+    Private Sub txtKembalian_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtKembalian.TextChanged
+        Dim value As Integer
+        value = txtKembalian.Text
+        txtKembalian.Text = Format(value, "##,##0")
+    End Sub
+
+    Private Sub txtPinjamBuku_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtPinjamBuku.TextChanged
+        Dim value As Integer
+        Dim HargaPinjaman As Double
+
+        If txtPinjamBuku.Text = String.Empty Then
+            txtPinjamBuku.Text = "0"
+        Else
+            value = txtPinjamBuku.Text
+            txtPinjamBuku.Text = Format(value, "##,##0")
+            txtPinjamBuku.SelectionStart = Len(txtPinjamBuku.Text)
+            HargaPinjaman = txtPinjamBuku.Text
+            txtTotalBayar.Text = HargaPinjaman + txtTotalDenda.Text
+        End If
+    End Sub
+
+    Private Sub txtUangDibayarkan_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtUangDibayarkan.TextChanged
+        Dim value As Integer
+
+        If txtUangDibayarkan.Text = String.Empty Then
+            txtUangDibayarkan.Text = "0"
+        Else
+            value = txtUangDibayarkan.Text
+            txtUangDibayarkan.Text = Format(value, "##,##0")
+            txtUangDibayarkan.SelectionStart = Len(txtUangDibayarkan.Text)
+            cmdHasil.Enabled = True
+        End If
+    End Sub
+
+    Private Sub txtTotalBayar_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtTotalBayar.TextChanged
+        Dim value As Integer
+        value = txtTotalBayar.Text
+        txtTotalBayar.Text = Format(value, "##,##0")
+    End Sub
+
+    Private Sub txtAnggota_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtAnggota.KeyPress
+        If Not ((e.KeyChar >= "0" And e.KeyChar <= "50") Or e.KeyChar = vbBack) Then e.Handled = True
+    End Sub
+
+    Private Sub txtKTP_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtKTP.KeyPress
+        If Not ((e.KeyChar >= "0" And e.KeyChar <= "50") Or e.KeyChar = vbBack) Then e.Handled = True
+    End Sub
+
+    Private Sub txtNama_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtNama.KeyPress
+        Dim unacceptable As String = "0123456789"
+
+        If InStr(unacceptable, e.KeyChar) > 0 Then
+            e.KeyChar = Nothing
+        End If
+    End Sub
+
+    Private Sub txtBukudipinjam_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtBukudipinjam.KeyPress
+        Dim unacceptable As String = "0123456789"
+
+        If InStr(unacceptable, e.KeyChar) > 0 Then
+            e.KeyChar = Nothing
+        End If
+    End Sub
+
+    Private Sub txtPinjamBuku_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtPinjamBuku.KeyPress
+        If Not ((e.KeyChar >= "0" And e.KeyChar <= "50") Or e.KeyChar = vbBack) Then e.Handled = True
+    End Sub
+
+    Private Sub txtUangDibayarkan_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtUangDibayarkan.KeyPress
+        If Not ((e.KeyChar >= "0" And e.KeyChar <= "50") Or e.KeyChar = vbBack) Then e.Handled = True
+    End Sub
+End Class
